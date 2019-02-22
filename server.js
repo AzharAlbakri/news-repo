1241745cb1ec4274b382506327739a88b945e76e
AzharAlbakri <albakriazhar.work@gmail.com>
2019-02-22 20:03:43 +0200

    modified package json and add dist to deploy

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname+'/dist'))
 
 app.use(function (req, res, next) {
 
